aa9f3cbaf838b0f68546779e5720313920608f4b
eerosale <stephanie.annrosales@gmail.com>
2015-03-28 20:10:39 -0700

    Interrupt pin
    Adds interrupt pin for flowsensor

--- talktomeprocessing/talktomeprocessing.ino
+++ talktomeprocessing/talktomeprocessing.ino
@@ -1,46 +1,65 @@
+
  int val; // Data received from the serial port
  int ledPin = 13; // Set the pin to digital I/O 13
-
+ int counter;
+ int state;
+ 
+ void count_me(){
+   counter++;
+ }
 void setup() 
 {
   pinMode(ledPin, OUTPUT);
-//initialize serial communications at a 9600 baud rate
+ // pinMode(FLOWSENSORPIN, INPUT);
+//  digitalWrite(FLOWSENSORPIN, HIGH);
+  attachInterrupt(0, count_me, RISING);
+  //initialize serial communications at a 9600 baud rate
 Serial.begin(9600);
 }
 
 void loop()
 {
+
   
-  if (Serial.available()) 
-   { // If data is available to read,
-     val = Serial.read(); // read it and store it in val
+   if (Serial.available()) 
+     { // If data is available to read,
+      val = Serial.read(); // read it and store it in val
    
-   switch (val) 
-   { // If 1 was received
-      case '0':         
-       Shot_one();
-        break; 
-      case '1':
-      Shot_two();
-      break;
-      case '2':
-      Shot_three();
-      break;
-      default:
-      break;
-   }
-}
+     switch (val) 
+     { // If 1 was received
+        case '0':         
+          Shot_one();
+          break; 
+        case '1':
+          Shot_two();
+          break;
+        case '2':
+          Shot_three();
+          break;
+        default:
+          break;
+     }
+  
+  }
 }
   int Shot_one(){
    
-  digitalWrite(13, HIGH);
-  delay(5000);
-  digitalWrite(13, LOW);
+  counter = 0;
+  
+ 
+  while(counter <5){
+    digitalWrite(13,HIGH);
+    //Serial.print(counter);
+  }
+  digitalWrite(13,LOW);
+  //digitalRead(FLOWSENSORPIN);
+  
     // turn the LED on (HIGH is the voltage level)
   
  }
  
  int Shot_two(){
+ 
    int i;
   for (i = 0; i<2; i++)
   {
@@ -55,9 +74,9 @@ void loop()
    int i;
   for (i = 0; i<5; i++)
   {
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(500);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
   delay(500);
  }
  }
